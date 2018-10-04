42a54d6930eb89f76f8966fcd546a12cdd89081f
SimplyRin <simplyrin.dev@gmail.com>
2018-10-04 23:44:42 +0900

    Add files via upload

---
+++ HttpClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+/**
+ * Created by SimplyRin on 2018/10/04.
+ */
+public class HttpClient {
+
+    public static String rawWithAgent(String url) {
+        return rawWithAgent(url, "Mozilla/5.0");
+    }
+    
+    public static String rawWithAgent(String url, String userAgent) {
+        HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+        httpWebRequest.UserAgent = userAgent;
+        httpWebRequest.Method = "GET";
+    
+        HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+    
+        Stream stream = httpWebResponse.GetResponseStream();
+        StreamReader streamReader = new StreamReader(stream);
+    
+        return streamReader.ReadToEnd();
+    }
+
+}
---
+++ ThreadPool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/**
+ * Created by SimplyRin on 2018/10/04.
+ */
+public class ThreadPool {
+
+    public static void runAsync(ThreadStart threadStart) {
+        new Thread(threadStart).Start();
+    }
+
+}
